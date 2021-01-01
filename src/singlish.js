dd172cc26a0a939a89969b11d18bff16352837a5
000407 <kanchana@senadheera.net>
2021-01-01 18:54:53 +0900

    Bug fix: corrected reference

--- src/singlish.js
+++ src/singlish.js
@@ -83,8 +83,8 @@ export default class Singlish {
 				if(text.charCodeAt(i_1) == 32) {
 					hal_able = 1;
 				}
-				else if(text.substr(i_1).match(SP_CHARS)) {
-					modifier = text.substr(i_1).match(SP_CHARS)[0];
+				else if(text.substr(i_1).match(Singlish.SP_CHARS)) {
+					modifier = text.substr(i_1).match(Singlish.SP_CHARS)[0];
 					translit += modifier;
 					hal_able = 0;
 				}
